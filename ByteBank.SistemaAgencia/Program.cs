4ad69aba2f5bb21aceb13f77f890791f91ac264b
Samih Freire <samihcisne@gmail.com>
2022-03-30 22:09:16 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta1 = new ContaCorrente(2121, 5534);
+
+            Console.WriteLine(conta1.Numero);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
