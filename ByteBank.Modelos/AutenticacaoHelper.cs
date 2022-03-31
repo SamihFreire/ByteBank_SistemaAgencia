b09bbebe75d1d4ff6a877ef505f15447a626ab8d
Samih Freire <samihcisne@gmail.com>
2022-03-31 17:30:18 -0300

    .

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    internal class AutenticacaoHelper //O modificador INTERNAL faz com que essa classe apenas seja vista na biblioteca que a contem
+    {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
