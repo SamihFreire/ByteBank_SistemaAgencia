8f03f33795b0b09d739ecbd2cafe369e25734a12
Samih Freire <samihcisne@gmail.com>
2022-04-05 17:58:03 -0300

    Aprendendo sobre string

---
+++ ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;  
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url nao pode ser nulo ou vazia.", nameof(url));
+            }
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            URL = url;
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,18 +12,42 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            DateTime dataFimPagamento = new DateTime(2022, 1, 20);
-            DateTime dataCorrente = DateTime.Now;
+            //DateTime dataFimPagamento = new DateTime(2022, 1, 20);
+            //DateTime dataCorrente = DateTime.Now;
 
             //TimeSpan diferenca = dataCorrente - dataFimPagamento;
-            TimeSpan diferenca = TimeSpan.FromMinutes(40);
-            string msg = "vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            //TimeSpan diferenca = TimeSpan.FromMinutes(40);
+            //string msg = "vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
 
-            Console.WriteLine(dataCorrente);
-            Console.WriteLine(dataFimPagamento);
+            //Console.WriteLine(dataCorrente);
+            //Console.WriteLine(dataFimPagamento);
 
-            Console.WriteLine(msg);
+            //Console.WriteLine(msg);
+
+            //ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(null);
+
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "kjdskldjsl";
+
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            
+
+            Console.ReadLine();
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?') + 1;
+
+            Console.WriteLine(url);
+
+            string argumentos = url.Substring(indiceInterrogacao);
+
+            Console.WriteLine(argumentos);
+
 
             Console.ReadLine();
             
