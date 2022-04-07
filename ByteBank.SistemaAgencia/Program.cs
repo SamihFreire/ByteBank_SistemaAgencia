fff5fa6dc0e89a4c9b2dcb73fa106ffe1f88657d
Samih Freire <samihcisne@gmail.com>
2022-04-06 21:00:48 -0300

    Aprenendo sobre a classe regex e colocando em prática Expressões Regulares

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using Humanizer;
@@ -12,6 +13,23 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            //Classe REGEX para Expressões Regulares
+
+            string padrao = "[0-9]{4,5}-?[0-9]{4}"; //{4, 5} Afirmando que aceita 4 ate 5 digitos na primeira parte / [-]{0,1} aceita sem [-] ou com [-], posso usar ? tbm para isso
+            string textoDeTeste = "Meu nome é guilherme, me ligue em 98784-4546";
+
+            Match match = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine("Existe o numero: " + Regex.IsMatch(textoDeTeste, padrao));
+            if(Regex.IsMatch(textoDeTeste, padrao)) 
+                Console.WriteLine("numero: " + match.Value);
+            Console.ReadLine();
+
+
+
+
+
+
             string urlParametros = "https://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             string urlTesteInicio = "https://www.bytebank.com";
             string urlTesteFim = "valor=1500";
